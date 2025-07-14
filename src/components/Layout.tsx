620066bd543e9ec9dcf7c363632039aeb62b6923
SophoGurabanidze <sogurabanidze@gmail.com>
2025-07-14 09:13:22 +0400

    feat: add ScrollToTop to Layout for better UX when navigating routes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,14 +3,20 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar'; 
 import Footer from './Footer';
 import { Toaster } from 'react-hot-toast'; 
+import ScrollToTop from './ScrollToTop'; 
 
 const Layout: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col">
+     
+      <ScrollToTop />
+
       <Navbar />
+
       <main className="flex-1">
         <Outlet />
       </main>
+
       <Footer />
 
       {/* ✅ Toaster INSIDE Router context */}
